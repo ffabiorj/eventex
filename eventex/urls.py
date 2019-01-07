ee354515d8308a2d934332d66703a9bdfd67f086
Fabio Oliveira <fabio20rj@gmail.com>
2019-01-07 15:00:33 -0200

    added template contact form

--- eventex/urls.py
+++ eventex/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from core.views import home
+from subscriptions.views import subscribe
 
 urlpatterns = [
     path('', home, name='index'),
+    path('inscricao/', subscribe, name='subscribe'),
     path('admin/', admin.site.urls),
 ]
