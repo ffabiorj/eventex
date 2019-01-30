3a907f341b748acd3b4efab120ba37ed9ff1e68d
Fabio Oliveira <fabio20rj@gmail.com>
2019-01-30 17:42:55 -0200

    Persite a inscrição

---
+++ subscriptions/tests/test_model_subscriptions.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from subscriptions.models import Subscription
+from datetime import datetime
+
+class SubscriptionModel(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Fabio Oliveira',
+            cpf='12345678901',
+            email='fabiorj@com.br',
+            phone='3333-2222'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr..."""
+        self.assertIsInstance(self.obj.created_at, datetime)
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.template.loader import render_to_string
 from subscriptions.forms import SubscriptionForm
+from subscriptions.models import Subscription
 from django.conf import settings
 
 
@@ -26,7 +27,7 @@ def create(request):
                 'subscriptions/subscription_email.txt',
                 form.cleaned_data)
     
-    
+    Subscription.objects.create(**form.cleaned_data)
     messages.success(request, 'Inscrição realizada com sucesso!')
     return HttpResponseRedirect('/inscricao/')
 
