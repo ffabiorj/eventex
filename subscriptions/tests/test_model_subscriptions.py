dba6c02422d55d2c4c350ddc0e76b6495b9825b3
Fabio Oliveira <fabio20rj@gmail.com>
2019-02-07 16:29:22 -0200

    add field paid

--- subscriptions/tests/test_model_subscriptions.py
+++ subscriptions/tests/test_model_subscriptions.py
@@ -21,3 +21,8 @@ class SubscriptionModel(TestCase):
 
     def test_str(self):
         self.assertEqual('Fabio Oliveira', str(self.obj))
+
+    def test_paid_default_to_False(self):
+        """By default paid must be False"""
+        self.assertEqual(False, self.obj.paid)
+
