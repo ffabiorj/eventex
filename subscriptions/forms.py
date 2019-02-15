9d69bd87fbd837466134e8e194ef3fef3e78709c
Fabio Oliveira <fabio20rj@gmail.com>
2019-02-15 19:26:40 -0200

    simplifica com modelform

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -1,21 +1,30 @@
 from django import forms
+from subscriptions.models import Subscription
+from subscriptions.validators import validate_cpf
 from django.core.exceptions import ValidationError
 
 
-def validate_cpf(value):
-    if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números.', 'digits')
-    if len(value) != 11:
-        raise ValidationError('CPF deve ter 11 números.', 'length')
 
-
-class SubscriptionForm(forms.Form):
+class SubscriptionFormOld(forms.Form):
     name = forms.CharField(label='None')
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
     email = forms.EmailField(label='Email', required=False)
     phone = forms.CharField(label='Telefone', required=False)
 
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone.')
+        return self.cleaned_data
+
+
+class SubscriptionForm(forms.ModelForm):
+
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
+
     def clean_name(self):
+        self.cleaned_data = super().clean()
         name = self.cleaned_data['name']
         words = [w.capitalize() for w in name.split()]
         return ' '.join(words)
